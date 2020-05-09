0cb4e32c123651709234d290b4da125ca44234c1
bhavuk-khanna <bhavukkhanna@gmail.com>
2020-05-09 16:32:07 +0530

    First Commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("What up bitches!!");
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port,function(err){
+    if(err)
+        console.log("Error Starting the server");
+    
+    console.log('Server has been started');
+});
